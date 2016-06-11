9d18feb74ef6efb83ccf7b56843c16478489b32d
Ante Borzić <aborzic@extensionengine.com>
2016-06-11 20:50:18 +0200

    First with vue-cli

---
+++ src/components/app/app.vue
@@ -0,0 +1,30 @@
+<script>
+  import template from './template.html';
+
+  export default {
+    template,
+    data() {
+      return {
+        newSearchTerm: '',
+        channels: [],
+      };
+    },
+    methods: {
+      newSubscription() {
+        this.channels.push({
+          term: this.newSearchTerm,
+          active: true,
+        });
+        this.newSearchTerm = '';
+      },
+      toggleSearch(toggledChannel) {
+        for (const channel of this.channels) {
+          if (toggledChannel.term === channel.term) {
+            channel.active = !channel.active;
+            // break;
+          }
+        }
+      },
+    },
+  };
+</script>
